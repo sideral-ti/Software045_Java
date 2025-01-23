dfe6e083e8f254afebed7ec46235685fe9ccc213
Sideral TI <sideralti.app@gmail.com>
2025-01-23 14:25:32 -0300

    initial project

---
+++ src/main/java/com/sideralti/app/SoftwareSpringMechardiniFastFoodApplication.java
@@ -0,0 +1,13 @@
+package com.sideralti.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareSpringMechardiniFastFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareSpringMechardiniFastFoodApplication.class, args);
+	}
+
+}
